24a1a76736a870268986eb748c50544b089ae3bd
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-09 23:38:50 -0600

    node js basics command line

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var message = 'Hello Node!';
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-var sum = 5 + 3;
 
-console.log(message);
-console.log(sum);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
