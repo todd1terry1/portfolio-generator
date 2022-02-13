486e49522e37e4795dd13c38e5fee7d85feb4113
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-13 03:48:49 -0600

    added recursion

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ const promptUser = () => {
   };
 
   const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+}
     console.log(`
   =================
   Add a New Project
@@ -70,10 +73,20 @@ const promptUser = () => {
         message: 'Would you like to enter another project?',
         default: false
       }
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
   };
   
+  
   promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers))
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
