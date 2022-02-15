e03fa945453e17764afa561f65a606410c7515dd
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-15 09:57:47 -0600

    spread

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,13 @@
-const generatePage = (name, github) => {
+module.exports = templateData => {
+    console.log(templateData);
+
+    // this will create three variables based on data in templateData
+const { projects, about, ...header } = templateData;
+  
     return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
+    <!DOCTYPE html>
+    <html lang="en">
+  
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +16,9 @@ const generatePage = (name, github) => {
     </head>
   
     <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
+      <h1>${templateData.name}</h1>
+      <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
     </body>
     </html>
     `;
   };
-
-  module.exports = generatePage;
